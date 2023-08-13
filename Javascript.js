f91a2761d6e638a7709377c9036b0638d93f75ca
Asim Ehmed <rocco.borocco95@gmail.com>
2023-08-13 16:06:59 +0400

    adding setTime

--- Javascript.js
+++ Javascript.js
@@ -11,6 +11,14 @@ let currentImg = 1;
 next.addEventListener("click", () => {
   currentImg++;
   console.log(currentImg);
+  clearTimeout(timeout);
+  updateImg();
+});
+
+prev.addEventListener("click", () => {
+  currentImg--;
+  console.log(currentImg);
+  clearTimeout(timeout);
   updateImg();
 });
 
@@ -19,8 +27,15 @@ next.addEventListener("click", () => {
 function updateImg() {
   if (currentImg > images.length) {
     currentImg = 1;
+  } else if (currentImg <= 0) {
+    currentImg = images.length;
   }
-
   image.style.transform = `translateX(-${(currentImg - 1) * 700}px)`;
   console.log(image.style.transform);
+  timeout = setTimeout(() => {
+    currentImg++;
+    updateImg();
+  }, 4000);
 }
+
+updateImg();
